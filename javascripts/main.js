060e416f75b33b608773a69b3f35f9a0f3441656
satish_mishra <sattasaphire@gmail.comm>
2015-01-04 01:16:12 +0530

    coming soon modified

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,4 +2,12 @@ angular.module('foodApp',[]);
 
 angular.module('foodApp').controller('foodController', function($scope){
 	$scope.rascal = 'Rascal_Inc';
+
+	$scope.fbPage = function(){
+		window.open("https://www.facebook.com/satish.mishra3");
+	}
+
+	$scope.twitterPage = function(){
+		window.open("https://twitter.com/Sattastudd");
+	}
 });
