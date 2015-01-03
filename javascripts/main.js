1a2d1dac4701169d74a488dd5648fd62e2e46abc
satish_mishra <sattasaphire@gmail.comm>
2015-01-04 03:47:46 +0530

    last modification fortoday

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 angular.module('foodApp',[]);
 
-angular.module('foodApp').controller('foodController', function($scope){
+angular.module('foodApp').controller('foodController', function($scope, $interval){
 	$scope.rascal = 'Rascal_Inc';
 
 	$scope.fbPage = function(){
@@ -10,4 +10,59 @@ angular.module('foodApp').controller('foodController', function($scope){
 	$scope.twitterPage = function(){
 		window.open("https://twitter.com/Sattastudd");
 	}
+
+	//console.log(ddToday);
+
+	$interval(showLeftTime, 1000);
+
+	function showLeftTime(){
+		var targetDays = 90;
+		var todayDate = new Date();
+		var ddToday = todayDate.getDate();
+		var mmToday = todayDate.getMonth();
+		var hhToday = todayDate.getHours();
+		var minToday = todayDate.getMinutes();
+		var secToday = todayDate.getSeconds();
+		var totalDaysOver = null;
+
+		if(mmToday === 0){
+			totalDaysOver = ddToday;
+		}
+		if(mmToday === 1){
+			totalDaysOver = 31+ddToday; 
+		}
+		if(mmToday === 2){
+			totalDaysOver = 59+ddToday;
+		}
+
+		$scope.daysLeft = targetDays - totalDaysOver;
+		$scope.hoursLeft = 24 - hhToday;
+		console.log(minToday);
+		$scope.minutesLeft = 60 - minToday;
+		$scope.secondsLeft = 60 - secToday;
+	
+		if($scope.secondsLeft < 10){
+			$scope.showZeroSeconds = true;
+			console.log($scope.showZeroSeconds);
+		}
+		else if($scope.secondsLeft > 10){
+			$scope.showZeroSeconds = false;
+		}
+		if($scope.minutesLeft < 10){
+			$scope.showZeroMinutes = true;
+		}
+		else if($scope.minutesLeft > 10){
+			$scope.showZeroMinutes = false;
+		}
+		if($scope.hoursLeft < 10){
+			$scope.showZeroHours = true;
+		}
+		else if($scope.hoursLeft > 10){
+			$scope.showZeroHours = false;
+		}
+		if($scope.daysLeft < 10){
+			$scope.showZeroDays = true;
+		}
+	}
+
 });
